741bfce1f850b88f706849957b03d4ed76ad2cae
Waseem-Mosam <waseem.mosam1@gmail.com>
2023-07-06 16:52:53 +0200

    Up to date

--- automation/settings.py
+++ automation/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-hvwn+*rhqi-xe$!2fyjfxi-m&_fc03px6py$dhfys_x2ei@s58
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
